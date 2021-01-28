1f21b7572382fe207a6fa446cc6bb030323e62cd
Simone Odegard <Simone.Odegard@gmail.com>
2021-01-28 12:21:18 -0800

    updated part of my broken code

--- app5.js
+++ app5.js
@@ -1,6 +1,6 @@
 function likeCats(){
     let userAnswer = prompt('Do you like cats? Yes or no?')
-    if (userAnswer != 'yes' && 'Yes'){
+    if (userAnswer != 'yes' && userAnswer != 'Yes'){
         alert('What are you doing here?')
     }
 }
